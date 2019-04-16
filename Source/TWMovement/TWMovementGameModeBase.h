1a46d225c59b759d8bc3297b4572f3492d4e5bdb
Chris <chrisrkemp@hotmail.co.uk>
2019-04-16 12:43:53 +0100

    Initial commit

---
+++ Source/TWMovement/TWMovementGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TWMovementGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWMOVEMENT_API ATWMovementGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
